7b03e5e3d04f6f143f72b4741e6ae8a780a7de9c
İsmail Özsaygı <ismailozsaygi@panteon.games>
2023-05-15 18:49:30 +0300

    Add: Added test code for SDL initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <SDL.h>
 
 int main( int argc, char* argv[] )
 {
-	std::cout << "Hello world!" << std::endl;
+	if ( SDL_Init( SDL_INIT_VIDEO ) != 0 )
+	{
+		std::cout << "Failed to initialize SDL!" << std::endl;
+		return -1;
+	}
+
+	SDL_Quit();
 
 	return 0;
 }
